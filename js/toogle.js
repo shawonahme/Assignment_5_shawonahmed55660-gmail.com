35ef70bd33fa520f5c00a2af43e5dc054281b8e4
shawonahme <shawonahmed55660@gmail.com>
2024-09-23 02:17:44 +0600

    delete one file which is not valid in this project

--- js/toogle.js
+++
@@ -1,9 +0,0 @@
-function toggleFuntion(myBtn,otherBtn){
-const myButton = document.getElementById(myBtn);
-const otherButton = document.getElementById(otherBtn);
-
-myButton.classList.add('bg-[#B4F461]')
-otherButton.classList.remove('bg-[#B4F461]')
-
-
-}
